71f498b0f657a075967175bff96def9a9ff51b34
Hapygirlie <happinesschinweopara@gmail.com>
2024-08-13 18:15:13 +0000

    0x03-user_authentication_service

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
-""" doc """
-
-from sqlalchemy import Column, Integer, String
+"""
+Declare a SQLAlchemy model named 'User' corresponding to a
+database table named "users"
+"""
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+)
 
 Base = declarative_base()
 
 
 class User(Base):
-    """document"""
-
+    """
+    Definition of class User
+    """
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
